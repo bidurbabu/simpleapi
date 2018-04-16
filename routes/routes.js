d1fb227f2f1c5200cb910e0820da73819ee9880a
Bidur Adhikari <badhikari@godaddy.com>
2018-04-16 16:04:26 -0700

    Adding logging to see the calls

--- routes/routes.js
+++ routes/routes.js
@@ -1,11 +1,14 @@
 var appRouter = function (app) {
   app.get("/", function(req, res) {
+      console.log('I got a call from '+ req.originalUrl);
     res.status(200).send("Welcome to our restful API");
   });
   app.get("/success", function(req, res) {
+    console.log('I got a successful call from '+ req.originalUrl);
     res.status(200).send("The message send was successful");
   });
   app.get("/failed", function(req, res) {
+      console.log('I got a failed call from '+ req.originalUrl);
     res.status(200).send("The message send failed");
   });
 }
