099b122f9a9df6c64545ad80dfe36aa751a64ffb
Bidur Adhikari <badhikari@godaddy.com>
2018-04-16 14:46:50 -0700

    Added success and failed message

--- routes/routes.js
+++ routes/routes.js
@@ -2,6 +2,12 @@ var appRouter = function (app) {
   app.get("/", function(req, res) {
     res.status(200).send("Welcome to our restful API");
   });
+  app.get("/success", function(req, res) {
+    res.status(200).send("The message send was successful");
+  });
+  app.get("/failed", function(req, res) {
+    res.status(200).send("The message send failed");
+  });
 }
 
 module.exports = appRouter;
